64e0fc0f782f95063b56c26daf8219ed7b677197
lh008 <paintluis@gmail.com>
2020-05-17 16:32:38 -0500

    Changed $.ajax() to $.get(), get worked faster

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -22,15 +22,12 @@ $(document).ready(function () {
     }
   });
 
-  $.ajax({
-    type: 'GET',
-    url = 'http://0.0.0.0:5001/api/v1/status/',
-    success: (function(data, textStatus) {
-      if (textStatus === 'OK') {
-	$('DIV#api_status').addClass('available');
-      } else {
-	$('DIV#api_status').removeClass('available');
-      }
-    });
+  const url = 'http://0.0.0.0:5001/api/v1/status/'
+  $.get (url, function(data) {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
   });
 });
